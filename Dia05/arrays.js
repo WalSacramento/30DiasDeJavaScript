2541cd4e58594e106f0ff150baa8101c97bb32f7
Waldsson Sacramento <waldsson3@gmail.com>
2022-11-19 23:55:30 -0300

    Add dia 05

---
+++ Dia05/arrays.js
@@ -0,0 +1,43 @@
+// Como criar arrays
+
+// Usando o construtor Array
+// // syntax
+// const arr = Array()
+// // or
+// // let arr = new Array()
+// console.log(arr) // []
+// Usando colchetes ([])
+// // syntax
+// // This the most recommended way to create an empty list
+// const arr = []
+// console.log(arr)
+
+// Como criar um array com valores
+// Array com valores iniciais. Usamos a propriedade length para encontrar o comprimento de um array.
+
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
+
+// Print the array and its length
+
+console.log('Numbers:' + numbers)
+console.log('Number of numbers:', numbers.length)
+
+console.log('Fruits:', fruits)
+console.log('Number of fruits:', fruits.length)
+
+console.log('Vegetables:', vegetables)
+console.log('Number of vegetables:', vegetables.length)
+
+console.log('Animal products:', animalProducts)
+console.log('Number of animal products:', animalProducts.length)
+
+console.log('Web technologies:', webTechs)
+console.log('Number of web technologies:', webTechs.length)
+
+console.log('Countries:', countries)
+console.log('Number of countries:', countries.length)
