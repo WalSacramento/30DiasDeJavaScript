ae4803cf35802999d87047993bbe2c09c0f3b788
Waldsson Sacramento <waldsson3@gmail.com>
2022-11-27 20:45:11 -0300

    Dia 09 Update

---
+++ Dia09/exercicio_nivel01.js
@@ -0,0 +1,42 @@
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+// Explique a diferença entre forEach, map, filter e reduce .
+
+
+// Defina uma função de retorno de chamada antes de usá-la em forEach, map, filter ou reduce.
+const callback = (Element, index, arr)
+
+// Use forEach para console.log cada país na matriz de países.
+// Use forEach para console.log cada nome na matriz de nomes.
+// Use forEach para console.log cada número na matriz de números.
+// Use map para criar uma nova matriz alterando cada país para letras maiúsculas na matriz de países.
+// Use o mapa para criar uma matriz de comprimento de países a partir da matriz de países.
+// Use map para criar uma nova matriz alterando cada número para quadrado na matriz de números
+// Use o mapa para alterar cada nome para letras maiúsculas na matriz de nomes
+// Use map para mapear a matriz de produtos para seus preços correspondentes.
+// Use o filtro para filtrar os países que contêm terras .
+// Use o filtro para filtrar os países com seis caracteres.
+// Use o filtro para filtrar os países que contêm seis letras ou mais na matriz de países.
+// Use o filtro para filtrar o país que começa com 'E';
+// Use o filtro para filtrar apenas os preços com valores.
+// Declare uma função chamada getStringLists que recebe uma matriz como parâmetro e retorna uma matriz apenas com itens de string.
+// Use reduza para somar todos os números na matriz de números.
+// Use a redução para concatenar todos os países e produzir esta frase: Estônia, Finlândia, Suécia, Dinamarca, Noruega e Islândia são países do norte da Europa
+// Explique a diferença entre alguns e todos
+// Use some para verificar se o comprimento de alguns nomes é maior que sete na matriz de nomes
+// Use cada para verificar se todos os países contêm a palavra terra
+// Explique a diferença entre find e findIndex .
+// Use find para encontrar o primeiro país contendo apenas seis letras na matriz de países
+// Use findIndex para encontrar a posição do primeiro país contendo apenas seis letras na matriz de países
+// Use findIndex para encontrar a posição da Noruega , se ela não existir na matriz, você obterá -1.
+// Use findIndex para encontrar a posição da Rússia , se ela não existir na matriz, você obterá -1.
--- Dia09/higher_order_functions.js
+++ Dia09/higher_order_functions.js
@@ -1,6 +1,20 @@
 // Um callback é uma função que pode ser passada como parâmetro para outra função. Veja o exemplo abaixo.
 
 // a callback function, the name of the function could be any name
+const callback = (n) => {
+  return n * 2
+}
+
+// function that takes other function as a callback
+function cube(callback, n) {
+  return callback(n) ** n
+}
+
+console.log(cube(callback, 3))
+
+// Um callback é uma função que pode ser passada como parâmetro para outra função. Veja o exemplo abaixo.
+
+// a callback function, the name of the function could be any name
 const callback1 = (n) => {
   return n ** 2
 }
@@ -12,16 +26,15 @@ function cube(callback, n) {
 ​
 console.log(cube(callback, 3))
 
-// Um callback é uma função que pode ser passada como parâmetro para outra função. Veja o exemplo abaixo.
-
-// a callback function, the name of the function could be any name
-const callback = (n) => {
-  return n ** 2
+// higher order
+// Higher order function returning an other function
+const higherOrder = n => {
+  const doSomething = m => {
+    const doWhatEver = t => {
+      return 2 * n + 3 * m + t
+    }
+    return doWhatEver
+  }
+  return doSomething
 }
-​
-// function that takes other function as a callback
-function cube(callback, n) {
-  return callback(n) * n
-}
-​
-console.log(cube(callback, 3))
+console.log(higherOrder(2)(3)(10))
